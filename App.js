3fa1f7b7234586d98883738bd73a4ab73a953859
repenaattori <reimarii@oamk.fi>
2022-09-06 11:02:49 +0300

    Init

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import DatePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+
+  const [show, setShow] =  useState(false);  
+  const [date, setDate] = useState(new Date());
+
+  //Function fow showing alert
+  const showAlert = () =>{
+  }
+
+  //Function for setting new date (onChange). 
+  //You can check the date and initate the alert here also
+  const setSelectedDate = (e, myDate)=>{
+  };
+
+  //You need DatePicker, button and textfield
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
     </View>
   );
 }
